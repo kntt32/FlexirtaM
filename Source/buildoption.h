fd616e2aa0822e3574bee00d30e0c812ea08d0a4
kntt32 <153593861+kntt32@users.noreply.github.com>
2024-04-18 18:29:42 +0900

    Update buildoption.h

--- Source/buildoption.h
+++ Source/buildoption.h
@@ -1,9 +1,9 @@
 #ifndef INCLUDED_BUILDOPTION
 #define INCLUDED_BUILDOPTION
 
-#define BASE_NUMBER_TYPE double
+#define BASE_NUMBER_TYPE float
     //数値の内部表記を設定
-#define BASE_NUMBER_CONVERT_OPARATER "%lf"
+#define BASE_NUMBER_CONVERT_OPARATER "%f"
     //BASE_NUMBER_TYPE型の変換演算子
 #define ENABLE_RDRAND 1
     //RDRAND命令を使用する　x64のみサポート
