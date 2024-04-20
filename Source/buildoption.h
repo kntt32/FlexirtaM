b66646c2b24e3fdb028f47317c01cbbef100b474
kntt32 <153593861+kntt32@users.noreply.github.com>
2024-04-20 21:24:37 +0900

    Update buildoption.h

--- Source/buildoption.h
+++ Source/buildoption.h
@@ -1,17 +1,33 @@
-#ifndef INCLUDED_BUILDOPTION
-#define INCLUDED_BUILDOPTION
+#pragma once
 
-#define BASE_NUMBER_TYPE float
-    //数値の内部表記を設定
-#define Matrix_Struct Matrix_##float
-    //型の名称
-#define Matrix_Method(name) Matrix_##float##_##name
-    //メゾッドの名前
-#define BASE_NUMBER_CONVERT_OPARATER "%f"
-    //BASE_NUMBER_TYPE型の変換演算子
-#define ENABLE_RDRAND 1
-    //RDRAND命令を使用する　x64のみサポート
-#define ENABLE_MULTITHREAD 1
-    //マルチスレッドを使用する未サポート
-
+#ifdef MATRIX_BASE_NUMBER_TYPE
+#undef MATRIX_BASE_NUMBER_TYPE
 #endif
+#ifdef Matrix_Struct
+#undef Matrix_Struct
+#endif
+#ifdef Matrix_Method
+#undef Matrix_Method
+#endif
+#ifdef MATRIX_BASE_NUMBER_CONVERT_OPARATER
+#undef MATRIX_BASE_NUMBER_CONVERT_OPARATER
+#endif
+#ifdef MATRIX_ENABLE_RDRAND
+#undef MATRIX_ENABLE_RDRAND
+#endif
+#ifdef MATRIX_ENABLE_MULTITHREAD
+#undef MATRIX_ENABLE_MULTITHREAD
+#endif
+
+#define MATRIX_BASE_NUMBER_TYPE double
+    //数値の内部表記を設定
+#define Matrix_Struct Matrix_##double
+    //型の名称
+#define Matrix_Method(name) Matrix_##double##_##name
+    //メゾッドの名前
+#define MATRIX_BASE_NUMBER_CONVERT_OPARATER "%lf"
+    //BASE_NUMBER_TYPE型の変換演算子
+#define MATRIX_ENABLE_RDRAND 1
+    //RDRAND命令を使用する　x64のみサポート
+#define MATRIX_ENABLE_MULTITHREAD 1
+    //マルチスレッドを使用する未サポート
